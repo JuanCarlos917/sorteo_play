8325ccec7aade79397f0828f0e469d04435f3825
Juan Gomez <juancarlos.gm931@gmail.com>
2024-07-11 19:21:19 -0500

    feat: css formUser

---
+++ frontEnd/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./index.html',
+		'./src/**/*.{vue,js,ts,jsx,tsx}',
+		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				grey300: '#e0e0e0',
+				black: '#000000',
+			},
+		},
+	},
+	plugins: [],
+};
+
