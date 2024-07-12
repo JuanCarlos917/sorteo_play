4b6bbb6fa1ec5293d18087450abd5f41d436bd36
Juan Gomez <juancarlos.gm931@gmail.com>
2024-07-12 17:49:24 -0500

    fix: new cors

--- backEnd/src/app.js
+++ backEnd/src/app.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const appRoutes = require('./routes/index');
 const authRoutes = require('./routes/adminRoutes.js');
-require('dotenv').config();
 require('./db.js');
 
 const server = express();
@@ -12,22 +12,8 @@ const server = express();
 server.name = 'API';
 
 // Configurar opciones de CORS
-const allowedOrigins = [
-	`${process.env.BASE_URL}`,
-	'https://otra-url-permitida.com',
-];
-
 const corsOptions = {
-	origin: (origin, callback) => {
-		// Allow requests with no origin, like mobile apps or curl requests
-		if (!origin) return callback(null, true);
-		if (allowedOrigins.indexOf(origin) === -1) {
-			const msg =
-				'The CORS policy for this site does not allow access from the specified Origin.';
-			return callback(new Error(msg), false);
-		}
-		return callback(null, true);
-	},
+	origin: process.env.BASE_URL,
 	credentials: true,
 	methods: 'GET, POST, OPTIONS, PUT, DELETE',
 	allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
