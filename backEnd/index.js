a0272fc2c432c991f2cf9ee5fb662a71ddd4636c
Juan Gomez <juancarlos.gm931@gmail.com>
2024-07-12 18:33:18 -0500

    fix(db): change Sequelize sync method to alter to prevent data loss

--- backEnd/index.js
+++ backEnd/index.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3001; // Si process.env.PORT no está definido, usa 3001 como valor predeterminado
 
-conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log(`Server is listening at port ${PORT}`);
-  });
+conn.sync({ alter: true }).then(() => {
+	server.listen(PORT, () => {
+		console.log(`Server is listening at port ${PORT}`);
+	});
 });
