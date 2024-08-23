0b9fbe58b244539dc2265662d781229a7b51ce91
Juan Gomez <juancarlos.gm931@gmail.com>
2024-08-23 16:10:03 -0500

    fix: Improve Docker database connection configuration, modularize connection code for various environments.

---
+++ backEnd/config/database.js
@@ -0,0 +1,42 @@
+// config/database.js
+require('dotenv').config();
+
+const getDatabaseConfig = (env) => {
+	switch (env) {
+		case 'development':
+			return {
+				name: process.env.DB_NAME_DEV,
+				user: process.env.DB_USER,
+				password: process.env.DB_PASSWORD,
+				host: process.env.DB_HOST,
+				port: process.env.DB_PORT,
+				dialectOptions: {},
+			};
+		case 'test':
+			return {
+				name: process.env.DB_NAME_TEST,
+				user: process.env.DB_USER,
+				password: process.env.DB_PASSWORD,
+				host: process.env.DB_HOST,
+				port: process.env.DB_PORT,
+				dialectOptions: {},
+			};
+		case 'production':
+		default:
+			return {
+				name: process.env.DB_PROD_NAME,
+				user: process.env.DB_PROD_USER,
+				password: process.env.DB_PROD_PASSWORD,
+				host: process.env.DB_PROD_HOST,
+				port: process.env.DB_PROD_PORT,
+				dialectOptions: {
+					ssl: {
+						require: true,
+						rejectUnauthorized: false,
+					},
+				},
+			};
+	}
+};
+
+module.exports = getDatabaseConfig;
--- backEnd/src/db.js
+++ backEnd/src/db.js
@@ -1,48 +1,20 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const getDatabaseConfig = require('../config/database');
 
-// Determina el entorno actual, por defecto es 'production'
 const environment = process.env.NODE_ENV || 'production';
+const dbConfig = getDatabaseConfig(environment);
 
-// Configuración de la base de datos dependiendo del entorno
-let DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, dialectOptions;
-
-if (environment === 'development') {
-	DB_NAME = process.env.DB_NAME_DEV;
-	DB_USER = process.env.DB_USER;
-	DB_PASSWORD = process.env.DB_PASSWORD;
-	DB_HOST = process.env.DB_HOST;
-	DB_PORT = process.env.DB_PORT;
-	dialectOptions = {};
-} else if (environment === 'test') {
-	DB_NAME = process.env.DB_NAME_TEST;
-	DB_USER = process.env.DB_USER;
-	DB_PASSWORD = process.env.DB_PASSWORD;
-	DB_HOST = process.env.DB_HOST;
-	DB_PORT = process.env.DB_PORT;
-	dialectOptions = {};
-} else {
-	// Producción por defecto
-	DB_NAME = process.env.DB_PROD_NAME;
-	DB_USER = process.env.DB_PROD_USER;
-	DB_PASSWORD = process.env.DB_PROD_PASSWORD;
-	DB_HOST = process.env.DB_PROD_HOST;
-	DB_PORT = process.env.DB_PROD_PORT;
-	dialectOptions = {
-		ssl: {
-			require: true,
-			rejectUnauthorized: false,
-		},
-	};
-}
-
-// Configura Sequelize
 const sequelize = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+	dbConfig.name,
+	dbConfig.user,
+	dbConfig.password,
 	{
+		host: dbConfig.host,
+		port: dbConfig.port,
+		dialect: 'postgres',
 		logging: false,
 		native: false,
-		dialectOptions,
+		dialectOptions: dbConfig.dialectOptions,
 	},
 );
 
